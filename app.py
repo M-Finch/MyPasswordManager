984f11d777538a72aeae78e339bb549b3c99de5b
Mister-Finch <m-finch-github@protonmail.com>
2021-02-07 10:02:30 +0100

    ajout password.svg et modif gitignore

--- app.py
+++ app.py
@@ -8,9 +8,14 @@ class App:
 	def __init__(self):
 		self.root = tkinter.Tk()
 		self.root.title('Générateur de password')
+		self.root.geometry("1080x720")
+		self.root.minsize(720, 480)
+
 
 
 myapp = App()
+myapp.root.mainloop()
+
 
 		
 		
